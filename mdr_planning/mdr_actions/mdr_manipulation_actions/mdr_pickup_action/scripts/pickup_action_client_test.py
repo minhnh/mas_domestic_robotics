8bf69b3ff4c9dd8ded1a48ee53f69d99f6c1a923
Roberto Mendieta <roberto.mendieta@smail.inf.h-brs.de>
2017-10-21 15:37:24 +0200

    Created mdr_pickup_action

---
+++ mdr_planning/mdr_actions/mdr_manipulation_actions/mdr_pickup_action/scripts/pickup_action_client_test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys
+
+import rospy
+import roslib
+import actionlib
+
+from mdr_pickup_action.msg import PickUpAction, PickUpGoal
+
+
+if __name__ == '__main__':
+    rospy.init_node('pickup_action_client_test')
+
+
+    client = actionlib.SimpleActionClient('pickup_server', PickUpAction)
+    client.wait_for_server()
+
+
+    goal = PickUpGoal()
+    # Fill in the goal here
+    goal.position.header.frame_id = 'base_link'
+    goal.position.header.stamp = rospy.Time.now()
+    goal.position.point.x = -0.7
+    goal.position.point.y =  0.0
+    goal.position.point.z =  0.9
+
+    client.send_goal(goal)
+    client.wait_for_result()
+    result = client.get_result()
+    
+    rospy.loginfo(result)
+
+    if result.success:
+        rospy.loginfo('Pickup succeeded')
+    else:
+        rospy.loginfo('Pickup failed')
