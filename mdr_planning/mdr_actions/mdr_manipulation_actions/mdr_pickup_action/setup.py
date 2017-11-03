86a88a62c679ddaef68f03e20350dd14577bd998
Roberto Mendieta <roberto.mendieta@smail.inf.h-brs.de>
2017-11-03 18:22:06 +0100

    Added ros folder, deleted unused kinematics.py and move_arm.py

--- mdr_planning/mdr_actions/mdr_manipulation_actions/mdr_pickup_action/setup.py
+++ mdr_planning/mdr_actions/mdr_manipulation_actions/mdr_pickup_action/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
    packages=['mdr_pickup_action'],
-   package_dir={'mdr_pickup_action': 'src/mdr_pickup_action'}
+   package_dir={'mdr_pickup_action': 'ros/src/mdr_pickup_action'}
 )
 
 setup(**d)
